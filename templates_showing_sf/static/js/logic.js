ad85ab599096adfe998a8f318958364ec097021f
qingyangucb <qingyangucb@gmail.com>
2020-01-08 21:25:31 -0800

    1.Fine tuned website look. 2.Added pop up and color change functionality when you click on the map layers. 3.Added click function to fill in starting and destination input field.

--- templates_showing_sf/static/js/logic.js
+++ templates_showing_sf/static/js/logic.js
@@ -3,15 +3,61 @@ var map = L.map("map", {
   zoom: 12});
 
 var streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY 
-}).addTo(map);
+	attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+	maxZoom: 18,
+	id: "mapbox.streets",
+	accessToken: API_KEY 
+})
+.addTo(map);
 
-console.log(SF_Grid);
-L.geoJSON(SF_Grid, {style: {color: "#4169E1"}}).addTo(map);
+var prevLayerClicked = null;
+var destination = null;
 
+function onEachFeature(feature, layer) {
+	layer.on({
+		click: function(e){
+			if (prevLayerClicked !== null) {
+				prevLayerClicked.setStyle({
+					weight: 2,
+					opacity: 1,
+					color: "#2d97d4",
+				});
+			}
+
+			var layer = e.target;
+			layer.setStyle({
+				weight: 2,
+				color: '#ff2400',
+				dashArray: '',
+			});
+
+			if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+				layer.bringToFront();
+			}
+			prevLayerClicked = layer;
+			destination = feature.properties.DISPLAY_NAME;
+		}
+	});
+	layer.bindPopup('<h5>' + feature.properties.DISPLAY_NAME +'</h5>');
+	
+}
+
+
+L.geoJSON(SF_Grid, {onEachFeature: onEachFeature, style: {color: "#2d97d4"}})
+.on("click", function (event) {
+	var clickedMarker = event.layer;
+	console.log('i clicked this')
+})
+// .bindPopup("Popup content")
+.addTo(map);
+
+function changestarting() {
+	document.getElementById("starting_id").value = destination;
+}
+
+function changedestination() {
+	document.getElementById("destination_id").value = destination;
+}
 // console.log(uniq);
 
 // var url = '/data_json';
